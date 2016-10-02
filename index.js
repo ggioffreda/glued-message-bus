1741650c8a47b0f112db20d2f0290aa40af360fd
Giovanni Gioffreda <giovanni.gioffreda@gmail.com>
2016-10-02 22:10:30 +1300

    Move classes to source directory.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-module.exports.MessageBusErrors = require('./message-bus-errors')
-module.exports.MessageBus = require('./message-bus')
-module.exports.MessageBusChannel = require('./message-bus-channel')
+module.exports.MessageBusErrors = require('./src/message-bus-errors')
+module.exports.MessageBus = require('./src/message-bus')
+module.exports.MessageBusChannel = require('./src/message-bus-channel')
